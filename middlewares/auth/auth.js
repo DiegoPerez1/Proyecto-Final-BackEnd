1577d402a84fe17eb8d0ab91af2ae7028c8f4412
Rodrigo Costa <ro.costa.dev@gmail.com>
2023-05-09 16:20:27 -0300

    search

--- middlewares/auth/auth.js
+++ middlewares/auth/auth.js
@@ -2,12 +2,14 @@ const jwt = require("jsonwebtoken");
 
 exports.verifyToken = (req, res, next) => {
   const token = req.header("Authorization");
+
   if (!token) {
     res.status(401).json({ error: "Acceso al recurso denegado" });
     return;
   }
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+
     req.user = verified;
     next();
   } catch (error) {
